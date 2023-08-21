7a76664a57ea8ec77d322de7c98214c8efd03f15
canamend <isaac.canamend@gmail.com>
2023-08-21 16:19:33 -0500

    Web server listo

--- app.js
+++ app.js
@@ -33,10 +33,6 @@ app.get('/elements', (req, res) => {
     });
 })
 
-app.get('*', (req, res) => {
-  res.sendFile( __dirname + '/public/404.html');
-})
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
